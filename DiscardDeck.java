0a193c9bda046bb74ae1d60b148d748dab49d383
Jayson Marshall <marsh208@mail.chapman.edu>
2016-12-05 00:12:33 -0800

    updated discardDeck

---
+++ DiscardDeck.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class DiscardDeck
+{
+    private ArrayList<Integer> discardDeck;
+
+    public DiscardDeck()
+    {
+        discardDeck = new ArrayList<>();
+    }
+
+    public ArrayList<Integer> getDiscardDeck()
+    {
+        return discardDeck;
+    }
+
+
+
+
+
+
+}
